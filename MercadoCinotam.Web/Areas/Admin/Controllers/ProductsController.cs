ff1af71353822b525697443a5b442549517d0d9a
Alan Torres <temp_peri@hotmail.com>
2016-06-23 15:43:56 -0500

    Inicial

---
+++ MercadoCinotam.Web/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using Abp.UI;
+using Abp.Web.Models;
+using Abp.Web.Mvc.Authorization;
+using Helpers.GenericTypes;
+using MercadoCinotam.Products.Admin;
+using MercadoCinotam.Products.Admin.Dtos;
+using MercadoCinotam.Web.Controllers;
+using System;
+using System.Web.Mvc;
+
+namespace MercadoCinotam.Web.Areas.Admin.Controllers
+{
+    [AbpMvcAuthorize]
+    public class ProductsController : MercadoCinotamControllerBase
+    {
+        private readonly IProductAdminService _productAdminService;
+
+        public ProductsController(IProductAdminService productAdminService)
+        {
+            _productAdminService = productAdminService;
+        }
+
+        // GET: Admin/Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [WrapResult(false)]
+        public JsonResult GetProducts(RequestModel model)
+        {
+            ProccessQueryData(model, new[] { "ProductName", "Sku" }, new[] { "Id", "Sku", "ProductName", "ProductPrice", "Active", "" });
+            var products = _productAdminService.GetProducts(model);
+            return Json(products, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult AddProduct(Guid? id)
+        {
+            var product = _productAdminService.GetProductForEdit(id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult AddProduct(ProductInput model)
+        {
+
+            ValidateModel(model);
+            if (Request.Files.Count <= 0)
+            {
+                throw new UserFriendlyException("Image no encontrada");
+            }
+            model.Imagen = Request.Files[0];
+            _productAdminService.AddProductToStore(model);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddGalardon(Guid? id)
+        {
+            var galardons = _productAdminService.GetGalardons(id);
+            return View(galardons);
+        }
+
+        public ActionResult AddGalardon(GalardonInput input)
+        {
+            var id = _productAdminService.AddGalardon(input);
+            return Json(id);
+        }
+
+    }
+}
